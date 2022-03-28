0e6b4c607d50a73a956e37c245513a2685d50dd0
Максим Толстобров <maximah1989@yandex.ru>
2022-03-28 10:43:27 +0300

    Create gradient

--- app/src/main/java/com/secondworld/globaltestproject/ui/MainActivity.kt
+++ app/src/main/java/com/secondworld/globaltestproject/ui/MainActivity.kt
@@ -1,19 +1,41 @@
 package com.secondworld.globaltestproject.ui
 
+import android.annotation.SuppressLint
+import android.content.res.Resources
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.secondworld.globaltestproject.R
+import com.secondworld.globaltestproject.core.createGradient
 import com.secondworld.globaltestproject.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     lateinit var binding: ActivityMainBinding
 
+    @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        binding.textTest1.text = "HELLO MY FRIEND"
+
+        createGradient(binding.textTest1, intArrayOf(
+            resources.getColor(R.color.orange_red),
+            resources.getColor(R.color.coral),
+            resources.getColor(R.color.blue_violet)
+        ))
+
+        binding.textTest2.text = "HELLO MY FRIEND"
+
+        createGradient(binding.textTest2, intArrayOf(
+            R.color.orange_red,
+            R.color.coral,
+            R.color.blue_violet
+        ))
+
 
     }
+
 }
