c426be859076a60611716efa131400c217f62bad
Максим Толстобров <maximah1989@yandex.ru>
2022-03-28 09:51:15 +0300

    Create palete

---
+++ app/src/main/java/com/secondworld/globaltestproject/core/Extension.kt
@@ -0,0 +1,54 @@
+package com.secondworld.globaltestproject.core
+
+import android.content.res.Resources
+import android.graphics.LinearGradient
+import android.graphics.Shader
+import android.util.Log
+import android.view.View
+import android.widget.ImageView
+import android.widget.TextView
+
+fun updateText(view: TextView, message: Any) {
+    view.text = message.toString()
+}
+
+fun View.show() {
+    visibility = View.VISIBLE
+}
+
+fun View.hide() {
+    visibility = View.GONE
+}
+
+fun View.enabled() {
+    isEnabled = true
+    isClickable = true
+}
+
+fun View.notEnabled() {
+    isEnabled = false
+    isClickable = false
+}
+
+fun showViews(vararg views: View) {
+    for (view in views) view.visibility = View.VISIBLE
+}
+
+fun hideViews(vararg views: View) {
+    for (view in views) view.visibility = View.GONE
+}
+
+fun log(message : String){
+    Log.d("TAG", "log: $message")
+}
+
+fun createGradient(textView: TextView, colors: IntArray) {
+    val paint = textView.paint
+    val width = paint.measureText(textView.text.toString())
+    val textShader: Shader = LinearGradient(0f, 0f, width, textView.textSize, colors, null, Shader.TileMode.REPEAT)
+
+    textView.paint.shader = textShader
+}
+
+fun Int.toDp() : Int = (this / Resources.getSystem().displayMetrics.density).toInt()
+fun Int.toPx() : Int = (this * Resources.getSystem().displayMetrics.density).toInt()
---
+++ app/src/main/java/com/secondworld/globaltestproject/ui/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.secondworld.globaltestproject.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.secondworld.globaltestproject.R
+import com.secondworld.globaltestproject.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+    }
+}
