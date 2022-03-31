47afa42c09a2d9d39cbf87ab0d070a277bfc1e62
Максим Толстобров <maximah1989@yandex.ru>
2022-03-31 09:17:10 +0300

    Check

--- app/src/main/java/com/secondworld/globaltestproject/FirstFragment.kt
+++ app/src/main/java/com/secondworld/globaltestproject/FirstFragment.kt
@@ -1,15 +1,11 @@
 package com.secondworld.globaltestproject
 
-import android.animation.AnimatorSet
-import android.animation.ObjectAnimator
 import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.View
-import android.view.animation.AnimationSet
 import androidx.navigation.fragment.findNavController
 import com.secondworld.globaltestproject.core.animateLikeButton
-import com.secondworld.globaltestproject.core.snackbar
 import com.secondworld.globaltestproject.databinding.FragmentFirstBinding
 
 class FirstFragment : Fragment(R.layout.fragment_first) {
@@ -27,6 +23,8 @@ class FirstFragment : Fragment(R.layout.fragment_first) {
         }
 
         binding.textShadow.animateLikeButton()
+
+
 
     }
 
