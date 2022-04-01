c7c033495b8f85009089adebfedb987fdaf3b6a1
Максим Толстобров <maximah1989@yandex.ru>
2022-04-01 07:29:49 +0300

    Check

--- app/src/main/java/com/secondworld/globaltestproject/ui/MainActivity.kt
+++ app/src/main/java/com/secondworld/globaltestproject/ui/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.secondworld.globaltestproject.ui
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.secondworld.globaltestproject.R
@@ -9,11 +10,15 @@ class MainActivity : AppCompatActivity() {
 
     lateinit var binding: ActivityMainBinding
 
+    @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        binding.textTest.text = "hiiiiii".upperFirst()
 
     }
 }
+
+fun String.upperFirst() = this.replaceFirstChar { char -> char.uppercase() }
