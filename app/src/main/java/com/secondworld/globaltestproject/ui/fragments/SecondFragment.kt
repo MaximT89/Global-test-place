f0ed0f8a8fda95f2e13909f9607a1de72942cd45
BuenasDias <tmaksim1989@gmail.com>
2022-04-02 11:33:23 +0300

    Check

--- app/src/main/java/com/secondworld/globaltestproject/ui/fragments/SecondFragment.kt
+++ app/src/main/java/com/secondworld/globaltestproject/ui/fragments/SecondFragment.kt
@@ -3,11 +3,8 @@ package com.secondworld.globaltestproject.ui.fragments
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.View
-import androidx.fragment.app.setFragmentResultListener
 import androidx.navigation.fragment.findNavController
 import com.secondworld.globaltestproject.R
-import com.secondworld.globaltestproject.core.updateText
-import com.secondworld.globaltestproject.data.Person
 import com.secondworld.globaltestproject.databinding.FragmentSecondBinding
 
 class SecondFragment : Fragment(R.layout.fragment_second) {
@@ -18,13 +15,6 @@ class SecondFragment : Fragment(R.layout.fragment_second) {
         super.onViewCreated(view, savedInstanceState)
         val binding = FragmentSecondBinding.bind(view)
         bindingFragment = binding
-
-        setFragmentResultListener("key1") { _, bundle ->
-            val result = bundle.getString("data1")
-            val person = bundle.getParcelable<Person>("data2")
-//            result?.let { updateText(binding.textTest, it) }
-            person?.let { updateText(binding.textTest, it.name) }
-        }
 
         binding.btnNext.setOnClickListener {
             findNavController().navigate(R.id.action_secondFragment_to_thirdFragment)
