cdfc04c7213922ee1cce99f0215463a2e466f220
BuenasDias <tmaksim1989@gmail.com>
2022-04-20 09:45:45 +0300

    check

--- app/src/main/java/com/secondworld/globaltestproject/data/models/Person.kt
+++
@@ -1,3 +0,0 @@
-package com.secondworld.globaltestproject.data.models
-
-class Person(val name : String, val age : Int)
--- app/src/main/java/com/secondworld/globaltestproject/ui/MainActivity.kt
+++ app/src/main/java/com/secondworld/globaltestproject/ui/MainActivity.kt
@@ -1,62 +1,31 @@
 package com.secondworld.globaltestproject.ui
 
-import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.activity.viewModels
-import com.secondworld.globaltestproject.data.repository.RepositoryImpl
-import com.secondworld.globaltestproject.data.storages.StorageName
+import com.secondworld.globaltestproject.core.updateText
+import com.secondworld.globaltestproject.data.repository.Repository
+import com.secondworld.globaltestproject.data.storage.StoragePerson
 import com.secondworld.globaltestproject.databinding.ActivityMainBinding
-import com.secondworld.globaltestproject.domain.repository.Repository
-import com.secondworld.globaltestproject.domain.useCases.PersonUseCase
+import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
-    private val storageName = StorageName()
-    private val repository: Repository = RepositoryImpl(storageName)
-    private val personAdapter = PersonAdapter()
-    private val viewModel: MainViewModel by viewModels {
-        ViewModelFactory(PersonUseCase(repository))
-    }
 
-    @SuppressLint("SetTextI18n")
+    private val viewModel by viewModels<MainViewModel>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        initView()
-        initData()
-        initObservers()
-    }
-
-    private fun initObservers() {
-        viewModel.listPerson.observe(this) {
-            if (it != null) {
-                personAdapter.items = it
-            }
-        }
-    }
-
-    private fun initData() {
-
-        viewModel.getPerson()
-
-        personAdapter.callBackPerson = { position, _ ->
-            viewModel.removePerson(position)
+        viewModel.persons.observe(this){
+            updateText(binding.textPersonName, it[45].name)
         }
 
     }
-
-    private fun initView() {
-
-        binding.recyclerView.apply {
-            setHasFixedSize(true)
-            adapter = personAdapter
-        }
-    }
-
-
 }
 
