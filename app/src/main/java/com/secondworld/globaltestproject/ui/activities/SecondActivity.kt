1a9564805d945dbd37d72897528782dc5961a324
MaximT89 <maximah1989@yandex.ru>
2022-04-23 10:44:40 +0300

    check

--- app/src/main/java/com/secondworld/globaltestproject/ui/activities/SecondActivity.kt
+++ app/src/main/java/com/secondworld/globaltestproject/ui/activities/SecondActivity.kt
@@ -4,14 +4,21 @@ import android.animation.Animator
 import android.animation.AnimatorSet
 import android.animation.ObjectAnimator
 import android.annotation.SuppressLint
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.view.animation.Animation
 import android.view.animation.Animation.INFINITE
+import androidx.lifecycle.lifecycleScope
 import com.secondworld.globaltestproject.R
 import com.secondworld.globaltestproject.databinding.ActivitySecondBinding
 import com.secondworld.globaltestproject.ui.AnimatorGenerator
+import com.secondworld.globaltestproject.ui.MainActivity
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 class SecondActivity : AppCompatActivity() {
 
@@ -28,11 +35,28 @@ class SecondActivity : AppCompatActivity() {
     }
 
     private fun initGodClass() {
-        val animatorGenerator = AnimatorGenerator().apply {
-            nextAnimationSet(listAnimatorSet[0]){
-                nextAnimationSet(listAnimatorSet[1]){
-
+        AnimatorGenerator().apply {
+            nextAnimationSet(listAnimatorSet[0]) {
+                nextAnimationSet(listAnimatorSet[1]) {
+                    nextAnimationSet(listAnimatorSet[2]){
+                        nextAnimationSet(listAnimatorSet[3]){
+                            nextAnimationSet(listAnimatorSet[4]){
+                                delayAndDo {
+                                    startActivity(Intent(this@SecondActivity, MainActivity::class.java))
+                                }
+                            }
+                        }
+                    }
                 }
+            }
+        }
+    }
+
+    private fun delayAndDo(doSome : () -> Unit?){
+        lifecycleScope.launch(Dispatchers.IO){
+            delay(1000)
+            withContext(Dispatchers.Main){
+                doSome.invoke()
             }
         }
     }
@@ -40,22 +64,43 @@ class SecondActivity : AppCompatActivity() {
     @SuppressLint("Recycle")
     private fun initAnimators() {
 
-        val rotate = ObjectAnimator.ofFloat(binding.backLogo, View.ROTATION, 0f, 360f)
-        val scaleX = ObjectAnimator.ofFloat(binding.backLogo, View.SCALE_X, 1f, 2f, 1f)
-        val scaleY = ObjectAnimator.ofFloat(binding.backLogo, View.SCALE_Y, 1f, 2f, 1f)
+        val rotate = ObjectAnimator.ofFloat(binding.backLogo, View.ROTATION, 0f, 720f)
+        val scaleX = ObjectAnimator.ofFloat(binding.logoBlock, View.SCALE_X, 1f, 0.5f)
+        val scaleY = ObjectAnimator.ofFloat(binding.logoBlock, View.SCALE_Y, 1f, 0.5f)
+        val translationY = ObjectAnimator.ofFloat(binding.logoBlock, View.TRANSLATION_Y, 1f, -580f)
+        val alphaText = ObjectAnimator.ofFloat(binding.appName, View.ALPHA, 0f, 1f)
+        val alphaBlackWindow = ObjectAnimator.ofFloat(binding.blackWindow, View.ALPHA, 0f, 1f)
 
         val animatorSet1 = AnimatorSet().apply {
-            duration = 2000
+            duration = 3500
             play(rotate)
         }
 
         val animatorSet2 = AnimatorSet().apply {
-            duration = 10000
+            duration = 2500
             playTogether(scaleX, scaleY)
+        }
+
+        val animatorSet3 = AnimatorSet().apply {
+            duration = 3000
+            play(translationY)
+        }
+
+        val animatorSet4 = AnimatorSet().apply {
+            duration = 2500
+            play(alphaText)
+        }
+
+        val animatorSet5 = AnimatorSet().apply {
+            duration = 2500
+            play(alphaBlackWindow)
         }
 
         listAnimatorSet.add(animatorSet1)
         listAnimatorSet.add(animatorSet2)
+        listAnimatorSet.add(animatorSet3)
+        listAnimatorSet.add(animatorSet4)
+        listAnimatorSet.add(animatorSet5)
 
 
     }
