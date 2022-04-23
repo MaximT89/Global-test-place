8c0928c638b0e17862d9436bbfac18c755f8e282
MaximT89 <maximah1989@yandex.ru>
2022-04-23 15:23:10 +0300

    check

--- app/src/main/java/com/secondworld/globaltestproject/ui/MainActivity.kt
+++ app/src/main/java/com/secondworld/globaltestproject/ui/MainActivity.kt
@@ -3,10 +3,13 @@ package com.secondworld.globaltestproject.ui
 import android.animation.AnimatorSet
 import android.animation.ObjectAnimator
 import android.annotation.SuppressLint
+import android.media.MediaPlayer
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.ImageView
+import androidx.annotation.RequiresApi
 import com.bumptech.glide.Glide
 import com.secondworld.globaltestproject.R
 import com.secondworld.globaltestproject.core.updateText
@@ -21,8 +24,9 @@ class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     private val storageName = StorageName()
     private val repository: Repository = RepositoryImpl(storageName)
+    private var mediaPlayer: MediaPlayer? = null
 
-    @SuppressLint("SetTextI18n")
+    @SuppressLint("SetTextI18n", "NewApi")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
@@ -31,7 +35,38 @@ class MainActivity : AppCompatActivity() {
         initView()
     }
 
+    @SuppressLint("NewApi")
     private fun initView() {
+
+        binding.btnStart.setOnClickListener {
+            startPlayMusic()
+        }
+
+        binding.btnStop.setOnClickListener {
+            stopPlayMusic()
+        }
+
+
+    }
+
+    private fun stopPlayMusic() {
+        if (mediaPlayer?.isPlaying == true) {
+            mediaPlayer?.release()
+            mediaPlayer = null
+        }
+    }
+
+    private fun startPlayMusic() {
+
+//        if(mediaPlayer != null){
+//            if(mediaPlayer?.isPlaying == true){
+//                stopPlayMusic()
+//            }
+//        }
+
+        mediaPlayer = MediaPlayer.create(this, R.raw.bird)
+        mediaPlayer?.prepare()
+        mediaPlayer?.start()
 
     }
 }
