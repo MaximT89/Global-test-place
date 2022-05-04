d3fbc3f0ff34b3c46e5fabcaf70752eea19088fe
Максим Толстобров <maximah1989@yandex.ru>
2022-05-04 14:57:20 +0300

    Check

--- app/src/main/java/com/secondworld/globaltestproject/ui/animals/MainActivity.kt
+++ app/src/main/java/com/secondworld/globaltestproject/ui/animals/MainActivity.kt
@@ -1,7 +1,5 @@
 package com.secondworld.globaltestproject.ui.animals
 
-import android.annotation.SuppressLint
-import android.util.Log
 import android.view.LayoutInflater
 import androidx.activity.viewModels
 import androidx.core.view.isGone
@@ -12,12 +10,8 @@ import com.secondworld.globaltestproject.core.BaseActivity
 import com.secondworld.globaltestproject.core.launchWhenStarted
 import com.secondworld.globaltestproject.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
-import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.flow.onEach
 
-const val TAG = "TAG"
-
-@SuppressLint("ShowToast")
 @AndroidEntryPoint
 class MainActivity : BaseActivity<ActivityMainBinding>() {
 
@@ -30,8 +24,8 @@ class MainActivity : BaseActivity<ActivityMainBinding>() {
     override fun initObservers() {
         viewModel.state.onEach {
             when(it){
-                is MainActivityState.Error -> Snackbar.make(this, binding.root, it.error, Snackbar.LENGTH_LONG)
-                is MainActivityState.Init -> Snackbar.make(this, binding.root, "Стартанули", Snackbar.LENGTH_LONG)
+                is MainActivityState.Error -> Snackbar.make(this, binding.root, it.error, Snackbar.LENGTH_LONG).show()
+                is MainActivityState.Init -> Snackbar.make(this, binding.root, "Стартанули", Snackbar.LENGTH_LONG).show()
                 is MainActivityState.IsLoading -> {
                     if(it.isLoading){
                         binding.shimmerViewContainer.isVisible = true
