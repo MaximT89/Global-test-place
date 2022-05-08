0ee826d1bcd437f40323820aa791d032b0a37cdc
MaximT89 <maximah1989@yandex.ru>
2022-05-08 14:24:31 +0300

    check

--- app/src/main/java/com/secondworld/globaltestproject/ui/App.kt
+++ app/src/main/java/com/secondworld/globaltestproject/ui/App.kt
@@ -1,7 +1,26 @@
 package com.secondworld.globaltestproject.ui
 
 import android.app.Application
-import dagger.hilt.android.HiltAndroidApp
+import android.content.Context
+import com.secondworld.globaltestproject.data.user.cache.room.AppDataBase
+import com.secondworld.globaltestproject.data.user.cache.room.UserDao
+import dagger.hilt.android.qualifiers.ApplicationContext
+import javax.inject.Singleton
 
-@HiltAndroidApp
-class App : Application()
+class App : Application(){
+
+    var userDao : UserDao? = null
+
+    companion object{
+        var INSTANCE : App? = null
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+
+        INSTANCE = this
+        userDao = AppDataBase.getDatabase(this)?.userDao()
+    }
+
+    fun userDao() = userDao
+}
