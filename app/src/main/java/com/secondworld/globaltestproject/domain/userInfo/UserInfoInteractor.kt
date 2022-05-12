e0b2877f23f41ad2daabb5e8eb790239c2bd8afa
Максим Толстобров <maximah1989@yandex.ru>
2022-05-12 09:51:24 +0300

    Check

--- app/src/main/java/com/secondworld/globaltestproject/domain/userInfo/UserInfoInteractor.kt
+++ app/src/main/java/com/secondworld/globaltestproject/domain/userInfo/UserInfoInteractor.kt
@@ -2,7 +2,6 @@ package com.secondworld.globaltestproject.domain.userInfo
 
 import javax.inject.Inject
 
-
 class UserInfoInteractor @Inject constructor(private val repositoryUserInfo: RepositoryUserInfo) {
 
     fun execute() : UserInfo {
