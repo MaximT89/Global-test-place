5f00b5e97a858af33f1774f1dbef149c1f7fa96d
Максим Толстобров <maximah1989@yandex.ru>
2022-05-19 13:07:17 +0300

    Check

--- app/src/main/java/com/secondworld/globaltestproject/di/AppModule.kt
+++ app/src/main/java/com/secondworld/globaltestproject/di/AppModule.kt
@@ -1,10 +1,16 @@
 package com.secondworld.globaltestproject.di
 
+import com.secondworld.globaltestproject.data.repository.RepositoryImpl
+import com.secondworld.globaltestproject.data.storages.StorageName
+import com.secondworld.globaltestproject.domain.repository.Repository
 import com.secondworld.globaltestproject.ui.CommunicationFoo
 import dagger.Binds
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import java.net.ResponseCache
+import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
