e3649f5afe8e3c16a788e5d490bd5ba9291069bc
BuenasDias <tmaksim1989@gmail.com>
2022-05-26 18:47:02 +0300

    commit 1

--- app/src/main/java/com/secondworld/globaltestproject/ui/MainViewModel.kt
+++ app/src/main/java/com/secondworld/globaltestproject/ui/MainViewModel.kt
@@ -5,6 +5,7 @@ import androidx.lifecycle.viewModelScope
 import com.secondworld.globaltestproject.data.repository.RepositoryImpl
 import com.secondworld.globaltestproject.domain.models.Animals
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.*
 import kotlinx.coroutines.launch
 import javax.inject.Inject
@@ -20,7 +21,7 @@ class MainViewModel @Inject constructor(private val repository: RepositoryImpl) 
     }
 
     private fun fetchAnimals() {
-        viewModelScope.launch {
+        viewModelScope.launch(Dispatchers.IO) {
             _animals.emit(repository.generateAnimals())
         }
     }
