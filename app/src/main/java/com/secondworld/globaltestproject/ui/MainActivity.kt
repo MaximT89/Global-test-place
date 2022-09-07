8b073478e01ccd1bc7296a714e47bda7424d3483
Максим Толстобров <maximah1989@yandex.ru>
2022-09-07 10:55:15 +0300

    Refactoring

--- app/src/main/java/com/secondworld/globaltestproject/ui/MainActivity.kt
+++ app/src/main/java/com/secondworld/globaltestproject/ui/MainActivity.kt
@@ -1,10 +1,12 @@
 package com.secondworld.globaltestproject.ui
 
-import android.annotation.SuppressLint
+
+import android.app.Activity
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.activity.viewModels
-import androidx.lifecycle.ViewModelProvider
+import androidx.core.view.isGone
 import com.secondworld.globaltestproject.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -14,13 +16,33 @@ class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     private val viewModel : MainViewModel by viewModels()
 
+    companion object {
+        private const val REQUEST_SELECT_IMAGE_IN_ALBUM = 1
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        viewModel.observe(this){
-            binding.textName.text = it.name
+        binding.rootImage.setOnClickListener {
+            selectImageInAlbum()
+        }
+    }
+
+    private fun selectImageInAlbum() {
+        val intent = Intent(Intent.ACTION_GET_CONTENT)
+        intent.type = "image/*"
+        if (intent.resolveActivity(packageManager) != null) {
+            startActivityForResult(intent, REQUEST_SELECT_IMAGE_IN_ALBUM)
+        }
+    }
+
+    @Deprecated("Deprecated in Java")
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        if(resultCode == Activity.RESULT_OK && requestCode == REQUEST_SELECT_IMAGE_IN_ALBUM){
+            binding.imgViewPhoto.setImageURI(data?.data)
+            binding.placeholderImage.isGone = true
         }
     }
 }
--- app/src/main/java/com/secondworld/globaltestproject/ui/MainViewModel.kt
+++ app/src/main/java/com/secondworld/globaltestproject/ui/MainViewModel.kt
@@ -1,25 +1,11 @@
 package com.secondworld.globaltestproject.ui
 
-import androidx.lifecycle.LifecycleOwner
-import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
 @HiltViewModel
-class MainViewModel @Inject constructor(
-    private val communicationFoo: CommunicationFoo
-) : ViewModel(), Observe<Foo> {
+class MainViewModel @Inject constructor() : ViewModel() {
 
-    override fun observe(owner: LifecycleOwner, observer: Observer<Foo>) {
-        communicationFoo.observe(owner, observer)
-    }
 
-    init {
-        fetchDataFromInteractor()
-    }
-
-    private fun fetchDataFromInteractor(){
-        communicationFoo.map(Foo("Foo"))
-    }
 }
