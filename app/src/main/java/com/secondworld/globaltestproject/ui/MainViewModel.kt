7b741e9367023ea689e6f9861929dbfd8d5d84f4
Максим Толстобров <maximah1989@yandex.ru>
2022-09-09 09:21:00 +0300

    Refactoring

--- app/src/main/java/com/secondworld/globaltestproject/ui/MainViewModel.kt
+++ app/src/main/java/com/secondworld/globaltestproject/ui/MainViewModel.kt
@@ -1,11 +1,14 @@
 package com.secondworld.globaltestproject.ui
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
 @HiltViewModel
 class MainViewModel @Inject constructor() : ViewModel() {
-
 
 }
