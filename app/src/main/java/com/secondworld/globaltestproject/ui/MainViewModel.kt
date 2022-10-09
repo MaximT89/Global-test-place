05997856a87e3abf28047add99b84cdffa8f1452
BuenasDias <tmaksim1989@gmail.com>
2022-10-09 11:42:41 +0300

    create timer

--- app/src/main/java/com/secondworld/globaltestproject/ui/MainViewModel.kt
+++ app/src/main/java/com/secondworld/globaltestproject/ui/MainViewModel.kt
@@ -4,6 +4,7 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 import javax.inject.Inject
@@ -11,4 +12,10 @@ import javax.inject.Inject
 @HiltViewModel
 class MainViewModel @Inject constructor() : ViewModel() {
 
+
+    fun test(){
+        viewModelScope.launch(Dispatchers.IO){
+
+        }
+    }
 }
