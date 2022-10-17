aa049ba43c8a5d1cd4995114a434973c1baa31d7
MaximT89 <maximah1989@yandex.ru>
2022-10-17 19:46:07 +0300

    update

--- app/src/main/java/com/secondworld/globaltestproject/ui/MainActivity.kt
+++ app/src/main/java/com/secondworld/globaltestproject/ui/MainActivity.kt
@@ -3,6 +3,7 @@ package com.secondworld.globaltestproject.ui
 import android.view.LayoutInflater
 import androidx.activity.viewModels
 import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
 import com.secondworld.globaltestproject.R
 import com.secondworld.globaltestproject.core.BaseActivity
@@ -23,7 +24,11 @@ class MainActivity : BaseActivity<ActivityMainBinding>() {
 
     override fun initView() {
 
-        binding.bottomNavView.setupWithNavController(findNavController(R.id.nav_host_fragment))
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment?
+        val navController = navHostFragment!!.navController
+
+        binding.bottomNavView.setupWithNavController(navController)
 
     }
 
