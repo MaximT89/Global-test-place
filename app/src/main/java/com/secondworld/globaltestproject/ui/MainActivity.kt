b6c73ade6925e76d43af6d8de385e20f45aa0a5e
BuenasDias <tmaksim1989@gmail.com>
2022-11-29 21:12:45 +0300

    refactor

--- app/src/main/java/com/secondworld/globaltestproject/ui/MainActivity.kt
+++ app/src/main/java/com/secondworld/globaltestproject/ui/MainActivity.kt
@@ -1,24 +1,33 @@
 package com.secondworld.globaltestproject.ui
 
+import android.R.attr
 import android.app.Activity
-import android.content.ActivityNotFoundException
 import android.content.ContentValues
 import android.content.Intent
 import android.graphics.Bitmap
 import android.net.Uri
+import android.os.Build
 import android.os.Bundle
+import android.os.Environment
 import android.provider.MediaStore
 import android.provider.MediaStore.Images
 import androidx.activity.viewModels
+import androidx.annotation.RequiresApi
+import androidx.core.content.FileProvider
 import com.bumptech.glide.Glide
 import com.google.android.material.snackbar.Snackbar
 import com.secondworld.globaltestproject.core.bases.BaseActivity
-import com.secondworld.globaltestproject.core.common.click
-import com.secondworld.globaltestproject.core.common.hide
-import com.secondworld.globaltestproject.core.common.show
+import com.secondworld.globaltestproject.core.common.*
 import com.secondworld.globaltestproject.data.remote.ResponseImageLoad
 import com.secondworld.globaltestproject.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import java.io.File
+import java.io.FileOutputStream
+import java.io.OutputStream
+import java.lang.Exception
 
 
 @AndroidEntryPoint
@@ -29,6 +38,9 @@ class MainActivity : BaseActivity() {
 
     private val REQUEST_TAKE_PHOTO = 1
     private val SELECT_PICTURE = 2
+
+    private val TAKE_PICTURE_REQUEST = 3
+    private var outputFileUri: Uri? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -49,15 +61,52 @@ class MainActivity : BaseActivity() {
             getImage()
         }
 
+        binding.btnCamera.click {
 
-        binding.btnSaveImg.click {
-            val takePictureIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+            val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+
             try {
-                startActivityForResult(takePictureIntent, REQUEST_TAKE_PHOTO)
-            } catch (e: ActivityNotFoundException) {
+                val file = File(
+                    getExternalFilesDir(Environment.DIRECTORY_PICTURES),
+                    "IMG_${System.currentTimeMillis()}.jpg"
+                )
+                outputFileUri = FileProvider.getUriForFile(
+                    this,
+                    "com.secondworld.globaltestproject.provider", file
+                )
+                intent.putExtra(MediaStore.EXTRA_OUTPUT, outputFileUri)
+                startActivityForResult(intent, TAKE_PICTURE_REQUEST)
+
+            } catch (e : Exception){
+                log("error on start")
                 e.printStackTrace()
             }
         }
+    }
+
+    @RequiresApi(Build.VERSION_CODES.Q)
+    private fun saveInGalleryQ(bitmap: Bitmap) {
+
+        val filename = "IMG_${System.currentTimeMillis()}.jpg"
+        var fos: OutputStream?
+        var imageUri: Uri?
+        val contentValues = ContentValues().apply {
+            put(MediaStore.MediaColumns.DISPLAY_NAME, filename)
+            put(MediaStore.MediaColumns.MIME_TYPE, "image/jpg")
+            put(MediaStore.MediaColumns.RELATIVE_PATH, Environment.DIRECTORY_PICTURES)
+            put(MediaStore.Video.Media.IS_PENDING, 1)
+        }
+
+        contentResolver.also { resolver ->
+            imageUri = resolver.insert(Images.Media.EXTERNAL_CONTENT_URI, contentValues)
+            fos = imageUri?.let { resolver.openOutputStream(it) }
+        }
+
+        fos?.use { bitmap.compress(Bitmap.CompressFormat.JPEG, 100, it) }
+
+        contentValues.clear()
+        contentValues.put(MediaStore.Video.Media.IS_PENDING, 0)
+        contentResolver.update(imageUri!!, contentValues, null, null)
     }
 
     private fun getImage() {
@@ -76,41 +125,42 @@ class MainActivity : BaseActivity() {
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
 
-        // Другой вариант с применением when
         when (requestCode) {
-            REQUEST_TAKE_PHOTO -> {
-                if (resultCode == Activity.RESULT_OK && data !== null) {
-                    binding.imageMain.setImageBitmap(data.extras?.get("data") as Bitmap)
+            TAKE_PICTURE_REQUEST -> {
+                if (resultCode == Activity.RESULT_OK) {
 
-                    saveInGallery(data.extras?.get("data") as Bitmap)
-                }
+                    binding.imageMain.setImageURI(outputFileUri)
+
+//                    Glide.with(this).load(data.extras?.get("data") as Bitmap).into(binding.imageMain)
+//                    binding.imageMain.setImageBitmap(data.extras?.get("data") as Bitmap)
+//                    savePhoto(data.extras?.get("data") as Bitmap)
+
+                } else showSnackbar("Wrong request code")
             }
-            SELECT_PICTURE -> {
-                val selectedImageUri: Uri? = data?.data
-                Glide.with(this).load(selectedImageUri).into(binding.imageMain)
-            }
-            else -> {
-                showSnackbar("Wrong request code")
-            }
+            SELECT_PICTURE -> Glide.with(this).load(data?.data).into(binding.imageMain)
+            else -> showSnackbar("Wrong request code")
         }
     }
 
-    private fun saveInGallery(bitmap: Bitmap) {
-        Images.Media.insertImage(
-            this.contentResolver,
-            bitmap,
-            "new_photo",
-            "description"
-        )
+    private fun savePhoto(bitmap: Bitmap) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) saveInGalleryQ(bitmap)
+        else saveTheImageLegacyStyle(bitmap)
     }
 
-    fun addImageToGallery(filePath: String?) {
-        val values = ContentValues()
-        values.put(Images.Media.DATE_TAKEN, System.currentTimeMillis())
-        values.put(Images.Media.MIME_TYPE, "image/jpeg")
-        values.put(MediaStore.MediaColumns.DATA, filePath)
-        this.contentResolver.insert(Images.Media.EXTERNAL_CONTENT_URI, values)
+//    private fun savePhoto(bitmap: Bitmap) {
+//        Images.Media.insertImage(contentResolver, bitmap, "IMG_${System.currentTimeMillis()}.jpg", "sdasdasd")
+//    }
+
+    private fun saveTheImageLegacyStyle(bitmap: Bitmap) {
+
+        val filename = "IMG_${System.currentTimeMillis()}.jpg"
+        val imagesDir =
+            Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES)
+        val image = File(imagesDir, filename)
+        val fos: OutputStream = FileOutputStream(image)
+        fos.use { bitmap.compress(Bitmap.CompressFormat.JPEG, 100, it) }
     }
+
 
     private fun showSnackbar(message: String) {
         Snackbar.make(binding.root, message, Snackbar.LENGTH_LONG).show()
@@ -141,6 +191,15 @@ class MainActivity : BaseActivity() {
 
         if (data != null) {
             Glide.with(this).load(data.message).into(binding.imageMain)
+
+            CoroutineScope(Dispatchers.IO).launch {
+                val bitmap: Bitmap =
+                    Glide.with(this@MainActivity).asBitmap().load(data.message).submit().get()
+
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) saveInGalleryQ(bitmap)
+                else saveTheImageLegacyStyle(bitmap)
+            }
+
         }
     }
 }
