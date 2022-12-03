5bb5276ced42417bf62c11ee2fd8c7067fa677f6
BuenasDias <tmaksim1989@gmail.com>
2022-12-03 16:24:07 +0300

    refactor

---
+++ app/src/main/java/com/secondworld/globaltestproject/ui/BarChartActivity.kt
@@ -0,0 +1,48 @@
+package com.secondworld.globaltestproject.ui
+
+import android.graphics.Color
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+import com.github.mikephil.charting.data.BarData
+import com.github.mikephil.charting.data.BarDataSet
+import com.github.mikephil.charting.data.BarEntry
+import com.secondworld.globaltestproject.R
+import com.secondworld.globaltestproject.databinding.ActivityBarChartBinding
+
+class BarChartActivity : AppCompatActivity() {
+
+    private val binding: ActivityBarChartBinding by lazy {
+        ActivityBarChartBinding.inflate(layoutInflater)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+
+        val list: MutableList<BarEntry> = mutableListOf()
+        list.add(BarEntry(2014f, 420f))
+        list.add(BarEntry(2015f, 475f))
+        list.add(BarEntry(2016f, 508f))
+        list.add(BarEntry(2017f, 660f))
+        list.add(BarEntry(2018f, 550f))
+        list.add(BarEntry(2019f, 630f))
+        list.add(BarEntry(2020f, 470f))
+
+        val barDataSet = BarDataSet(list, "Visitors")
+        barDataSet.setColors(
+                ContextCompat.getColor(this, R.color.green),
+                ContextCompat.getColor(this, R.color.orange),
+                ContextCompat.getColor(this, R.color.maroon),
+                ContextCompat.getColor(this, R.color.Navy),
+        )
+        barDataSet.valueTextColor = Color.BLACK
+        barDataSet.valueTextSize = 16f
+
+        val barData = BarData(barDataSet)
+        binding.barChart.setFitBars(true)
+        binding.barChart.data = barData
+        binding.barChart.description.text = "Bar chart example"
+        binding.barChart.animateY(2000)
+    }
+}
--- app/src/main/java/com/secondworld/globaltestproject/ui/MainActivity.kt
+++ app/src/main/java/com/secondworld/globaltestproject/ui/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.secondworld.globaltestproject.ui
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.viewModels
 import com.secondworld.globaltestproject.core.BaseActivity
+import com.secondworld.globaltestproject.core.click
 import com.secondworld.globaltestproject.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -17,6 +19,10 @@ class MainActivity : BaseActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+
+        binding.btnBarChart.click {
+            startActivity(Intent(this, BarChartActivity::class.java))
+        }
 
     }
 }
