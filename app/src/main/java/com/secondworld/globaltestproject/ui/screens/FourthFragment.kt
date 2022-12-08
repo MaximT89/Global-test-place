7e274d33310722c5019d46489ccaaf37de970587
BuenasDias <tmaksim1989@gmail.com>
2022-12-08 21:17:31 +0300

    refactor test

--- app/src/main/java/com/secondworld/globaltestproject/ui/screens/FourthFragment.kt
+++ app/src/main/java/com/secondworld/globaltestproject/ui/screens/FourthFragment.kt
@@ -1,14 +1,17 @@
 package com.secondworld.globaltestproject.ui.screens
 
+import android.content.Context
+import androidx.activity.OnBackPressedCallback
 import com.secondworld.globaltestproject.R
 import com.secondworld.globaltestproject.core.BaseFragment
 import com.secondworld.globaltestproject.core.click
+import com.secondworld.globaltestproject.core.log
 import com.secondworld.globaltestproject.databinding.FragmentFouthBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class FourthFragment : BaseFragment<FragmentFouthBinding>(FragmentFouthBinding::inflate) {
-    override fun initView() = with(binding){
+    override fun initView() = with(binding) {
 
         btnGoThird.click {
             navigateTo(R.id.thirdFragment)
@@ -19,4 +22,21 @@ class FourthFragment : BaseFragment<FragmentFouthBinding>(FragmentFouthBinding::
 
     override fun initObservers() {
     }
+
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        val callback: OnBackPressedCallback = object : OnBackPressedCallback(
+            true // default to enabled
+        ) {
+            override fun handleOnBackPressed() {
+                log(tag = "b_press", message = "work work work")
+            }
+        }
+        requireActivity().onBackPressedDispatcher.addCallback(
+            this,  // LifecycleOwner
+            callback
+        )
+    }
+
 }
