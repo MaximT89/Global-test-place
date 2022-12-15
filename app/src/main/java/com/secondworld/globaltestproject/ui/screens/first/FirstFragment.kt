36f2fb2d38607ef899954e9a6c9d3a8c8ab3353b
MaximT89 <maximah1989@yandex.ru>
2022-12-15 08:44:32 +0300

    create first version

--- app/src/main/java/com/secondworld/globaltestproject/ui/screens/first/FirstFragment.kt
+++ app/src/main/java/com/secondworld/globaltestproject/ui/screens/first/FirstFragment.kt
@@ -1,7 +1,9 @@
 package com.secondworld.globaltestproject.ui.screens.first
 
+import android.widget.Toast
 import androidx.fragment.app.viewModels
 import com.secondworld.globaltestproject.core.bases.BaseFragment
+import com.secondworld.globaltestproject.core.extension.click
 import com.secondworld.globaltestproject.databinding.FragmentFirstBinding
 
 class FirstFragment :
@@ -10,7 +12,9 @@ class FirstFragment :
 
     override fun initView() = with(binding) {
 
-
+        testBtn.click {
+            Toast.makeText(requireActivity(), "текст", Toast.LENGTH_LONG).show()
+        }
     }
 
     override fun initObservers() {
