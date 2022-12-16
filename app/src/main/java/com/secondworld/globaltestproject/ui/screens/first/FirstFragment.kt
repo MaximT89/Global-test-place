0720ef48493aeee8f7f9ce369be011e0414f358d
BuenasDias <tmaksim1989@gmail.com>
2022-12-16 20:14:05 +0300

    refactor test

--- app/src/main/java/com/secondworld/globaltestproject/ui/screens/first/FirstFragment.kt
+++ app/src/main/java/com/secondworld/globaltestproject/ui/screens/first/FirstFragment.kt
@@ -1,19 +1,88 @@
 package com.secondworld.globaltestproject.ui.screens.first
 
+import android.text.Editable
+import android.text.TextUtils
+import android.text.TextWatcher
+import android.widget.EditText
 import androidx.fragment.app.viewModels
 import com.secondworld.globaltestproject.core.bases.BaseFragment
+import com.secondworld.globaltestproject.core.extension.click
+import com.secondworld.globaltestproject.core.extension.log
 import com.secondworld.globaltestproject.databinding.FragmentFirstBinding
+import com.secondworld.globaltestproject.ui.screens.first.Operation.*
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class FirstFragment :
     BaseFragment<FragmentFirstBinding, FirstViewModel>(FragmentFirstBinding::inflate) {
     override val viewModel: FirstViewModel by viewModels()
 
     override fun initView() = with(binding) {
+        editText.addTextChangedListener(MyTextWatcher(editText))
 
+        startPlus.click { viewModel.updateStartValue(PLUS) }
+        startMinus.click { viewModel.updateStartValue(MINUS) }
+
+        endPlus.click { viewModel.updateEndValue(PLUS) }
+        endMinus.click { viewModel.updateEndValue(MINUS) }
 
     }
 
-    override fun initObservers() {
+    override fun initObservers() = with(viewModel){
+
+        startValue.observe {
+            binding.editText.hint = viewModel.updateHint()
+            binding.startValueText.text = it.toString()
+            validateEditText()
+        }
+
+        endValue.observe {
+            binding.editText.hint = viewModel.updateHint()
+            binding.endValueText.text = it.toString()
+            validateEditText()
+        }
+    }
+
+    private fun validateEditText() {
+        if (!TextUtils.isEmpty(binding.editText.text) && (binding.editText.text.toString() != "") && (binding.editText.text.toString() != "-")) {
+            log("work1")
+            if (binding.editText.text.toString().toInt() > viewModel.getEndValue()!!) {
+                log("work2")
+                binding.editText.setText(viewModel.getEndValue()!!.toString())
+            }
+            if (binding.editText.text.toString().toInt() < viewModel.getStartValue()!!) {
+                log("work3")
+                binding.editText.setText(viewModel.getStartValue()!!.toString())
+            }
+        }
+    }
+
+    inner class MyTextWatcher(private val editText: EditText) : TextWatcher {
+        override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) = Unit
+
+        override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) = Unit
+
+        override fun afterTextChanged(input: Editable?) {
+
+            try {
+                editText.removeTextChangedListener(this)
+
+                if (input?.length!! >= 2 && input.startsWith("0"))
+                    editText.setText("")
+
+                if (input.toString().toInt() > viewModel.getEndValue()!!)
+                    editText.setText(viewModel.getEndValue().toString())
+
+                if (input.toString().toInt() < viewModel.getStartValue()!!)
+                    editText.setText(viewModel.getStartValue().toString())
+
+            } catch (e: Exception) {
+                editText.setText("0")
+            } finally {
+                editText.setSelection(editText.length())
+                editText.addTextChangedListener(this)
+            }
+        }
     }
 
 
--- app/src/main/java/com/secondworld/globaltestproject/ui/screens/first/FirstViewModel.kt
+++ app/src/main/java/com/secondworld/globaltestproject/ui/screens/first/FirstViewModel.kt
@@ -1,9 +1,49 @@
 package com.secondworld.globaltestproject.ui.screens.first
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import com.secondworld.globaltestproject.core.bases.BaseViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
 @HiltViewModel
 class FirstViewModel @Inject constructor() : BaseViewModel() {
+
+    private val _startValue = MutableLiveData(-20)
+    val startValue : LiveData<Int> = _startValue
+
+    private val _endValue = MutableLiveData(20)
+    val endValue : LiveData<Int> = _endValue
+
+    fun getStartValue() = _startValue.value
+
+    fun getEndValue() = _endValue.value
+
+    fun updateStartValue(operation : Operation){
+        when(operation){
+            Operation.PLUS -> {
+                val temp = _startValue.value?.plus(1)
+                if(temp!! < _endValue.value!!) _startValue.value = temp
+            }
+            Operation.MINUS -> _startValue.value = _startValue.value?.minus(1)
+        }
+    }
+
+    fun updateEndValue(operation : Operation){
+        when(operation){
+            Operation.PLUS -> _endValue.value = _endValue.value?.plus(1)
+            Operation.MINUS -> {
+                val temp = _endValue.value?.minus(1)
+                if(temp!! > _startValue.value!!) _endValue.value = temp
+            }
+        }
+    }
+
+    fun updateHint() = "от ${_startValue.value} до ${_endValue.value}"
+
+}
+
+enum class Operation{
+    PLUS,
+    MINUS
 }
