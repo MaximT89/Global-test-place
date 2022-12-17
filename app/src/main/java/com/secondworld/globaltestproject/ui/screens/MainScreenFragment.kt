3511c4ddaf555c0383dfd38a870d873b21a0ab7b
BuenasDias <tmaksim1989@gmail.com>
2022-12-17 21:03:42 +0300

    refactor test

--- app/src/main/java/com/secondworld/globaltestproject/ui/screens/MainScreenFragment.kt
+++ app/src/main/java/com/secondworld/globaltestproject/ui/screens/MainScreenFragment.kt
@@ -1,12 +1,17 @@
 package com.secondworld.globaltestproject.ui.screens
 
+import androidx.navigation.fragment.findNavController
+import com.secondworld.globaltestproject.R
 import com.secondworld.globaltestproject.core.BaseFragment
 import com.secondworld.globaltestproject.databinding.FragmentMainScreenBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainScreenFragment : BaseFragment<FragmentMainScreenBinding>(FragmentMainScreenBinding::inflate) {
-    override fun initView() {
+    override fun initView() = with(binding){
+        btnGoToSearch.setOnClickListener {
+            findNavController().navigate(R.id.searchScreenFragment)
+        }
 
     }
 
