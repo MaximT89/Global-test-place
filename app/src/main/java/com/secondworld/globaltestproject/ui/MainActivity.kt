0684d39ad4a0db0d5966152833ca349dd305afad
BuenasDias <tmaksim1989@gmail.com>
2022-12-17 23:22:14 +0300

    refactor test

--- app/src/main/java/com/secondworld/globaltestproject/ui/MainActivity.kt
+++ app/src/main/java/com/secondworld/globaltestproject/ui/MainActivity.kt
@@ -1,14 +1,18 @@
 package com.secondworld.globaltestproject.ui
 
-import android.os.Bundle
+import android.animation.ObjectAnimator
+import android.animation.PropertyValuesHolder
+import android.annotation.SuppressLint
+import android.content.Context
+import android.graphics.drawable.Drawable
 import android.view.LayoutInflater
+import android.view.MenuItem
+import android.view.View
 import androidx.activity.viewModels
-import androidx.navigation.NavController
-import androidx.navigation.NavDestination
-import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
-import androidx.navigation.fragment.findNavController
 import androidx.navigation.ui.setupWithNavController
+import com.airbnb.lottie.LottieCompositionFactory
+import com.airbnb.lottie.LottieDrawable
 import com.secondworld.globaltestproject.R
 import com.secondworld.globaltestproject.core.BaseActivity
 import com.secondworld.globaltestproject.core.log
@@ -32,15 +36,33 @@ class MainActivity : BaseActivity<ActivityMainBinding>() {
             supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment?
         val navController = navHostFragment!!.navController
 
-        navController.addOnDestinationChangedListener { controller, destination, arguments ->
-            log(message = "${destination.id}")
-            log(message = "${destination.label}")
+        binding.bottomNavView.setupWithNavController(navController)
 
-            binding.bottomNavView.selectedItemId = R.id.favouriteScreenFragment
+        binding.bottomNavView.menu.findItem(R.id.mainScreenFragment).icon =  getLottieDrawable(
+            context = this,
+            rawRes = R.raw.instagram
+        )
 
+        binding.bottomNavView.setOnItemSelectedListener { item ->
+            getIconDrawable(item.icon)
         }
+    }
 
-//        binding.bottomNavView.setupWithNavController(navController)
+    private fun getLottieDrawable(context: Context?, rawRes: Int): LottieDrawable {
+        return LottieDrawable().apply {
+            val result = LottieCompositionFactory.fromRawResSync(
+                context,
+                rawRes
+            )
+            composition = result.value
+        }
+    }
+
+    @SuppressLint("Recycle")
+    private fun getIconDrawable(icon: Drawable): Boolean{
+        val lottie = icon as? LottieDrawable
+        lottie?.playAnimation()
+        return true
     }
 
 }
