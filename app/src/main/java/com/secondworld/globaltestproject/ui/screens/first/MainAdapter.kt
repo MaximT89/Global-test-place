b25daa80142f8abe8649850804a4bf6684671fcf
MaximT89 <maximah1989@yandex.ru>
2023-02-26 22:06:14 +0300

    update

--- app/src/main/java/com/secondworld/globaltestproject/ui/screens/first/MainAdapter.kt
+++ app/src/main/java/com/secondworld/globaltestproject/ui/screens/first/MainAdapter.kt
@@ -1,7 +1,10 @@
 package com.secondworld.globaltestproject.ui.screens.first
 
+import android.app.ProgressDialog.show
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import com.secondworld.globaltestproject.core.extension.hide
+import com.secondworld.globaltestproject.core.extension.show
 import com.secondworld.globaltestproject.core.recycler.AbstractDiffCallback
 import com.secondworld.globaltestproject.core.recycler.BaseListAdapter
 import com.secondworld.globaltestproject.core.recycler.BaseViewHolder
@@ -13,6 +16,7 @@ class AnimalDiffUtilCallback : AbstractDiffCallback<Animal>()
 class MainAdapter : BaseListAdapter<Animal, MainAdapter.AnimalHolder>(
     AnimalDiffUtilCallback()
 ) {
+    val a = true
 
     inner class AnimalHolder(private val binding: HolderAnimalBinding) :
         BaseViewHolder<Animal>(binding) {
@@ -20,6 +24,8 @@ class MainAdapter : BaseListAdapter<Animal, MainAdapter.AnimalHolder>(
         override fun bind(item: Animal) {
             binding.name.text = item.name
             binding.age.text = item.age.toString()
+
+            binding.age.apply { if (a) show() else hide() }
         }
     }
 
